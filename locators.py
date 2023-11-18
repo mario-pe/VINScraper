3076b859a1fb814bac078c231d5630b5d0381ac1
Mariusz Perkowski <mariusz.perkowski@grapeup.com>
2023-11-18 18:06:25 +0100

    WebDriverWait transfoormation ended

--- locators.py
+++ locators.py
@@ -27,21 +27,4 @@ image_tab_xpath = "/html/body/div[7]/div[2]/div/div[1]/div/div/div[1]/div/div/sp
 google_image_link_radio_button_id = "c13"
 google_image_link_link_input_id = "c16"
 google_image_link_input_xpath = "//input[@placeholder='URL z&nbsp;wyników wyszukiwania']"
-
-def tests():
-    import undetected_chromedriver as uc
-    from selenium.webdriver import ActionChains, Keys
-    la = 'sds'
-    driver.sand_keys(link["link"])
-    driver.find_element(By.XPATH, "//span[text()='Wpisz słowo']").send_keys("2C3CDZBT5LH210515")
-    driver.find_element(By.XPATH, "/html/body/div[7]/div[2]/div/div[1]/div/div/div[2]/label/input").send_keys("2C3CDZBT5LH210515")
-    driver.find_element(By.XPATH, '//span[text()="Prześlij prośbę"]').click()
-
-    la.get_attribute("id")
-    driver = uc.Chrome()
-    action = ActionChains(driver)
-    action.context_click(la).send_keys(Keys.SHIFT, Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.RETURN).perform()
-    action.context_click(la).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.RETURN).perform()
-    action.context_click(la).send_keys(Keys.SHIFT, Keys.ARROW_DOWN).perform()
-    action.send_keys(Keys.SHIFT, Keys.ARROW_DOWN)
-    driver.find_element(By.XPATH, '//a[text()="Wszystko"]').click()
+request_send_correctly = "//span[text()='OK']"
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import ipdb
 import openpyxl
 import logging
@@ -27,30 +28,23 @@ from locators import (
     page_exists_block_address_xpath,
     image_tab_xpath,
     google_image_link_link_input_id,
-    google_image_link_radio_button_id, images_tab_xpath,
+    google_image_link_radio_button_id, images_tab_xpath, request_send_correctly,
 )
 
 BLUE_VIN = "2C3CDZBT5LH210515"
 
 
 PAGES_LINKS = []
-# IMAGES_LINKS = []
-IMAGES_LINKS = [
-    {
-        'image_link': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.bid.cars%2F70477833_652eda4397916%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945-6.jpg&tbnid=HcdDtrR9dBqfFM&vet=12ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygFegQIARBO..i&imgrefurl=https%3A%2F%2Fbid.cars%2Fen%2Flot%2F1-70477833%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945&docid=fjLoi3EFSk-s8M&w=640&h=480&q=1C4RJFBG4JC273945&hl=pl&ved=2ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygFegQIARBO',
-        'vin': '1C4RJFBG4JC273945'},
-    {
-        'image_link': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.bid.cars%2F70477833_652eda4397916%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945-10.jpg&tbnid=Cwcg8BQm58HZEM&vet=12ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygHegQIARBS..i&imgrefurl=https%3A%2F%2Fbid.cars%2Fen%2Flot%2F1-70477833%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945&docid=fjLoi3EFSk-s8M&w=640&h=480&q=1C4RJFBG4JC273945&hl=pl&ved=2ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygHegQIARBS',
-        'vin': '1C4RJFBG4JC273945'},
-    {
-        'image_link': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.bid.cars%2F70477833_652eda4397916%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945-7.jpg&tbnid=BzwNeHmhjI3-aM&vet=12ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygGegQIARBQ..i&imgrefurl=https%3A%2F%2Fbid.cars%2Fen%2Flot%2F1-70477833%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945&docid=fjLoi3EFSk-s8M&w=640&h=480&q=1C4RJFBG4JC273945&hl=pl&ved=2ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygGegQIARBQ',
-        'vin': '1C4RJFBG4JC273945'},
-    {
-        'image_link': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.bid.cars%2F70477833_652eda4397916%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945-9.jpg&tbnid=TaoEUYx9IhXmkM&vet=12ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygIegQIARBU..i&imgrefurl=https%3A%2F%2Fbid.cars%2Fen%2Flot%2F1-70477833%2F2018-Jeep-Grand-Cherokee-1C4RJFBG4JC273945&docid=fjLoi3EFSk-s8M&w=640&h=480&q=1C4RJFBG4JC273945&hl=pl&ved=2ahUKEwiQm671qr6CAxXOu6QKHRd8BLsQMygIegQIARBU',
-        'vin': '1C4RJFBG4JC273945'}
-]
+IMAGES_LINKS = []
 
-# VINS = []
+# VINS = [
+#     "1hd1krp14kb602715",
+#     "1hd1ydj16lb065180",
+#     "1hd1ktc14jb673480",
+#     "1hd1bxv14eb034736",
+#     "1hd1kef14mb640278",
+#     "1hd1jnv10eb023434",
+# ]
 VINS = [
     "WP1AB2A25FLA56998",
     "WBA8B3C50GK383844",
@@ -64,8 +58,6 @@ VINS = [
     "YV4162XZ7K2021323",
     "1C4RJFBGXMC701005",
 ]
-# VINS = ["1C6RR7FT3HS755769"]
-# VINS = ["WP1AB2A25FLA56998", "WBA8B3C50GK383844"]
 
 
 def load_vins():
@@ -86,15 +78,15 @@ def login(driver):
         WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, login_button)).click()
         user_name_input_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.NAME, next_username_input_name))
         user_name_input_element.send_keys(username)
-        WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, next_username_xpath)).click()
+        driver.find_element(By.XPATH, next_username_xpath).click()
         password_input_name_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.NAME, password_input_name))
         password_input_name_element.send_keys(password)
         WebDriverWait(driver, 10).until(lambda d: d.find_element(By.ID, password_next_button)).click()
     except Exception as e:
-        ipdb.set_trace()
-        logging.error(e)
-
-
+        sleep(1)
+        password_input_name_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.NAME, password_input_name))
+        password_input_name_element.send_keys(password)
+        WebDriverWait(driver, 10).until(lambda d: d.find_element(By.ID, password_next_button)).click()
 
 
 def first_search(driver):
@@ -117,22 +109,20 @@ def links_scrapper(driver, vin):
         href = la.get_attribute("href")
         if href:
             upper_href = href.upper()
-            if "bid.cars".upper() in upper_href and f"-{vin}" in upper_href:
+            if "bid.cars".upper() in upper_href and f"-{vin.upper()}" in upper_href:
                 links.append(href)
-            if "ucars.pro".upper() in upper_href and f"-{vin}" in upper_href:
+            if "ucars.pro".upper() in upper_href and f"-{vin.upper()}" in upper_href:
                 links.append(href)
-            if "stat.vin".upper() in upper_href and f"-{vin}" in upper_href:
+            if "stat.vin".upper() in upper_href and f"-{vin.upper()}" in upper_href:
                 links.append(href)
-            if "autohelperbot.com".upper() in upper_href and f"{vin}_" in upper_href:
+            if "autohelperbot.com".upper() in upper_href and f"{vin.upper()}_" in upper_href:
                 links.append(href)
-            if "plc.auction".upper() in upper_href and f"-{vin}-" in upper_href:
+            if "plc.auction".upper() in upper_href and f"-{vin.upper()}-" in upper_href:
                 links.append(href)
-            if "plc.ua".upper() in upper_href and f"-{vin}-" in upper_href:
+            if "plc.ua".upper() in upper_href and f"-{vin.upper()}-" in upper_href:
                 links.append(href)
     links = list(set(links))
-    list_dict = [{"vin": vin, "link": l} for l in links]
-    print(list_dict)
-    PAGES_LINKS.extend(list_dict)
+    return [{"vin": vin, "link": l} for l in links]
 
 
 def images_scrapper(driver, vin):
@@ -157,75 +147,97 @@ def images_scrapper(driver, vin):
         href = la.get_attribute("href")
         if href and "imgres" in href:
             upper_href = href.upper()
-            if "bid.cars".upper() in upper_href and f"-{vin}-" in upper_href:
+            if "bid.cars".upper() in upper_href and f"-{vin.upper()}-" in upper_href:
                 links.append(href)
-            if "ucars.pro".upper() in upper_href and f"-{vin}" in upper_href:
+            if "ucars.pro".upper() in upper_href and f"-{vin.upper()}" in upper_href:
                 links.append(href)
-            if "stat.vin" in upper_href and f"-{vin}" in upper_href:
+            if "stat.vin" in upper_href and f"-{vin.upper()}" in upper_href:
                 links.append(href)
-            if "autohelperbot.com".upper() in upper_href and f"{vin}_" in upper_href:
+            if "autohelperbot.com".upper() in upper_href and f"{vin.upper()}_" in upper_href:
                 links.append(href)
-            if "plc.auction".upper() in upper_href and f"-{vin}-" in upper_href:
+            if "plc.auction".upper() in upper_href and f"-{vin.upper()}-" in upper_href:
                 links.append(href)
-            if "plc.ua".upper() in upper_href and f"-{vin}-" in upper_href:
+            if "plc.ua".upper() in upper_href and f"-{vin.upper()}-" in upper_href:
                 links.append(href)
 
     links = list(set(links))
-    list_dict = [{"vin": vin, "image_link": l} for l in links]
-    print(list_dict)
-    IMAGES_LINKS.extend(list_dict)
+    return [{"vin": vin, "image_link": l} for l in links]
+
 
 
 def block_pages_request(driver, links):
     for link in links:
+        print(link["link"], link["vin"])
+
+        driver.get(
+            "https://search.google.com/search-console/remove-outdated-content"
+        )
+        button_new_block_request_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, button_new_block_request_xpath))
+        button_new_block_request_element.click()
+        input_block_address = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, input_block_address_xpath))
+        input_block_address.send_keys(link["link"])
+        # input_block_address.send_keys(link)  # zły link do odtworzenia sytuacji i zabezpieczenia NoSuchElementException
+        import ipdb; ipdb.set_trace()
+        driver.find_element(By.XPATH, button_send_block).click()
         try:
+            page_exists_block_address_button = WebDriverWait(driver, 1).until(lambda d: d.find_element(By.XPATH, page_exists_block_address_xpath))
+            page_exists_block_address_button.send_keys(link["vin"])
+        except Exception:
+            pass
+        action = ActionChains(driver)
+        action.send_keys(Keys.ESCAPE)
+        try:
+            driver.find_element(By.XPATH, button_send_block_request).click()
+        except Exception:
+            pass
+        sleep(0.5)
+        try:
+            sleep(1)
+            request_send_correctly_button = WebDriverWait(driver, 1).until(lambda d: d.find_element(By.XPATH, request_send_correctly))
+            request_send_correctly_button.click()
+        except Exception as e:
+            print(f"Exception  --------------------- block_pages_request")
             driver.get(
                 "https://search.google.com/search-console/remove-outdated-content"
             )
-            button_new_block_request_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, button_new_block_request_xpath))
-            button_new_block_request_element.click()
-            input_block_address = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, input_block_address_xpath))
-            input_block_address.send_keys(link["link"])
-            # input_block_address.send_keys(link)  # zły link do odtworzenia sytuacji i zabezpieczenia NoSuchElementException
-            driver.find_element(By.XPATH, button_send_block_request).click()
+            continue
 
-            driver.find_element(By.XPATH, page_exists_block_address_xpath).send_keys(
-                link["vin"]
-            )
-            action = ActionChains(driver)
-            action.send_keys(Keys.ESCAPE)
-            driver.find_element(By.XPATH, '//span[text()="Prześlij prośbę"]').click()
-        except Exception:
-            import ipdb
-
-            ipdb.set_trace()
 
 
 def block_images_request(driver, links):
     # driver.find_element(By.XPATH, button_new_block_request_xpath).click()
-    try:
         for link in links:
+            print(link["link"], link["vin"])
+            driver.get(
+                "https://search.google.com/search-console/remove-outdated-content"
+            )
             driver.get(REMOVE_CONTENT_URL)
             button_new_block_request_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, button_new_block_request_xpath))
             button_new_block_request_element.click()
 
             image_tab_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, image_tab_xpath))
             image_tab_element.click()
-
             google_image_link_radio_button_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.ID, google_image_link_radio_button_id))
             google_image_link_radio_button_element.click()
-            input_block_image = driver.find_element(
-                By.ID, google_image_link_link_input_id
-            )
+            input_block_image = driver.find_element(By.ID, google_image_link_link_input_id)
+
+            print(f" link zdjecia  -----------   {link['image_link']}")
             input_block_image.send_keys(link["image_link"])
             driver.find_element(By.XPATH, button_send_block).click()
-            send_request_button_element = WebDriverWait(driver, 10).until(
-                lambda d: d.find_element(By.XPATH, button_send_block_request))
-            send_request_button_element.click()
-    except Exception:
-        import ipdb
 
-        ipdb.set_trace()
+            try:
+                sleep(1)
+                request_send_correctly_button = WebDriverWait(driver, 2).until(lambda d: d.find_element(By.XPATH, request_send_correctly))
+                request_send_correctly_button.click()
+            except Exception as e:
+                print(f"Exception  --------------------- block_images_request")
+                driver.find_element(By.XPATH, '//span[text()="Prześlij prośbę"]').click()
+                try:
+                    request_send_correctly_button = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, request_send_correctly))
+                    request_send_correctly_button.click()
+                except Exception:
+                    pass
+                continue
 
 
 
@@ -241,59 +253,37 @@ def back_to_all_results(driver):
 
 
 if __name__ == "__main__":
+    import ipdb
     # load_vins()
-    try:
-        driver = init_driver()
-    except Exception:
-        import ipdb
+    driver = init_driver()
+    login(driver)
+    first_search(driver)
 
-        ipdb.set_trace()
+    for vin in VINS:
+        print(f"______________________________________________________vin: {vin}")
+        page_links = links_scrapper(driver, vin)
+        print(f"_____________________________________________________________________________page_links       len: {len(page_links)}")
+        print(f"______________________________________________________page_links: {page_links}")
+        images_links = images_scrapper(driver, vin)
+        print(f"_____________________________________________________________________________image_links      len: {len(images_links)}")
+        print(f"______________________________________________________image_links: {images_links}")
+        # block_pages_request(driver, page_links)
+        # block_images_request(driver, images_links)
+        back_to_all_results(driver)
 
-    try:
-        login(driver)
-    except Exception:
-        import ipdb
 
-        ipdb.set_trace()
 
+    # print(f"______________________________________________________IMAGES_LINKS len    { len(IMAGES_LINKS) }")
     # try:
-    #     first_search(driver)
+    #     block_images_request(driver, IMAGES_LINKS)
     # except Exception:
-    #     import ipdb
-    #
     #     ipdb.set_trace()
-    #
-    # for vin in VINS:
-    #     try:
-    #         links_scrapper(driver, vin)
-    #     except Exception:
-    #         import ipdb
-    #
-    #         ipdb.set_trace()
-    #
-    #     try:
-    #         images_scrapper(driver, vin)
-    #     except Exception:
-    #         import ipdb
-    #
-    #         ipdb.set_trace()
-    #
-    #     try:
-    #         back_to_all_results(driver)
-    #     except Exception:
-    #         import ipdb
-    #
-    #         ipdb.set_trace()
 
+    # print(f"______________________________________________________PAGES_LINKS len    {len(PAGES_LINKS)}")
     # try:
+    #
     #     block_pages_request(driver, PAGES_LINKS)
     # except Exception:
-    #     import ipdb
     #     ipdb.set_trace()
-    try:
-        block_images_request(driver, IMAGES_LINKS)
-    except Exception:
-        import ipdb
-        ipdb.set_trace()
-
+    ipdb.set_trace()
     driver.quit()

4a02193d766ad2aea806fea93ec2bc847fa50fee
Mariusz Perkowski <mariusz.perkowski@grapeup.com>
2023-12-05 15:50:43 +0100

    image link iter

--- main.py
+++ main.py
@@ -48,13 +48,13 @@ IMAGES_LINKS = []
 # ]
 
 VINS = [
-
- # '5UX83DP01N9L98571',
- # 'YV426MDB4F2590434',
- # '2C4RC1EG7JR237439',
- # '3FA6P0HD1JR222549',
- # 'JM1GL1VMXM1604596',
-# '5FNRL6H81PB062756',
+    # "LYV102RKXKB263123",
+    # "1FA6P8CF6K5186661",
+    # "1C4RJFBG6KC841906",
+    "YV426MDB4F2590434",
+    "YV4162XZ7K2021323",
+    "1C4RJFBGXMC701005",
+    "1C4RJFBG4JC273945",
 ]
 def load_vins():
     workbook = openpyxl.load_workbook(FILE_PATH)
@@ -65,7 +65,6 @@ def load_vins():
 
 def init_driver():
     return uc.Chrome()
-
 
 def login(driver):
     try:
@@ -90,15 +89,20 @@ def first_search(driver):
     search_textarea.send_keys("Dodge Challenger")
     search_textarea.submit()
 
+
 def back_to_all_results(driver):
     driver.execute_script("window.scrollTo(0, 0);")
     sleep(0.5)
     driver.find_element(By.XPATH, all_results_xpath).click()
 
+
 def new_search(driver, vin):
-    import ipdb; ipdb.set_trace()
     driver.get("https://www.google.com")
-    search_textarea = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.TAG_NAME, "textarea"))
+    try:
+        search_textarea = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.TAG_NAME, "textarea"))
+    except Exception:
+        driver.get("https://www.google.com")
+        search_textarea = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.TAG_NAME, "textarea"))
     search_textarea.send_keys(vin)
     search_textarea.submit()
 
@@ -127,6 +131,7 @@ def links_scrapper(driver, vin):
             if "plc.ua".upper() in upper_href and f"-{vin.upper()}-" in upper_href:
                 links.append(href)
     links = list(set(links))
+    [print({"vin": vin, "  link  ": l}) for l in links]
     return [{"vin": vin, "link": l} for l in links]
 
 
@@ -166,6 +171,7 @@ def images_scrapper(driver, vin):
                 links.append(href)
 
     links = list(set(links))
+    [print({"vin": vin, " image_link ": l}) for l in links]
     return [{"vin": vin, "image_link": l} for l in links]
 
 
@@ -173,10 +179,7 @@ def images_scrapper(driver, vin):
 def block_pages_request(driver, links):
     for link in links:
         print(link["link"], link["vin"])
-
-        driver.get(
-            "https://search.google.com/search-console/remove-outdated-content"
-        )
+        driver.get(REMOVE_CONTENT_URL)
         button_new_block_request_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, button_new_block_request_xpath))
         button_new_block_request_element.click()
         input_block_address = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, input_block_address_xpath))
@@ -191,19 +194,16 @@ def block_pages_request(driver, links):
         action = ActionChains(driver)
         action.send_keys(Keys.ESCAPE)
         try:
-            driver.find_element(By.XPATH, button_send_block_request).click()
+            send_request = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, button_send_block_request))
+            send_request.click()
         except Exception:
             pass
         sleep(0.5)
         try:
-            sleep(1)
             request_send_correctly_button = WebDriverWait(driver, 5).until(lambda d: d.find_element(By.XPATH, request_send_correctly))
             request_send_correctly_button.click()
         except Exception as e:
-            print(f"Exception  --------------------- block_pages_request")
-            driver.get(
-                "https://search.google.com/search-console/remove-outdated-content"
-            )
+            driver.get(REMOVE_CONTENT_URL)
             continue
 
 
@@ -213,28 +213,28 @@ def block_images_request(driver, links):
         for link in links:
             print(link["image_link"], link["vin"])
             driver.get(REMOVE_CONTENT_URL)
+            inputs = []
             button_new_block_request_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, button_new_block_request_xpath))
             button_new_block_request_element.click()
             try:
                 image_tab_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, image_tab_xpath))
                 image_tab_element.click()
             except Exception:
-                import ipdb; ipdb.set_trace()
-                image_tab_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, image_tab_xpath))
+                image_tab_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, image_tab_xpath))   # alternatywa ?
                 image_tab_element.click()
             try:
-                google_image_link_radio_button_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.ID, google_image_link_radio_button_id))
-                google_image_link_radio_button_element.click()
+                inputs = driver.find_elements(By.XPATH, "//input")
+                inputs[4].click()
             except Exception as e:
-                import ipdb; ipdb.set_trace()
-                google_image_link_radio_button_label = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, "//input[@placeholder='Wklej URL skopiowany za pomocą opcji „Kopiuj adres linku” w wynikach wyszukiwania grafiki']"))
-                google_image_link_radio_button_label.click()
+                print("Exception  ---------------------   Radio buton w Grafikach nie został znaleziony")
+                print(f'---- {link["image_link"]}  ------   {link["vin"]} --------')
             try:
-                input_block_image = driver.find_element(By.ID, google_image_link_link_input_id)
+                inputs[5].click()
+                inputs[5].send_keys(link["image_link"])
             except Exception as e:
-                import ipdb; ipdb.set_trace()
-                input_block_image = driver.find_element(By.XPATH, "/html/body/div[7]/div[2]/div/div[1]/div/div/div[3]/span/label[2]/div[2]/label/input")
-            input_block_image.send_keys(link["image_link"])
+                print("Exception  ---------------------   Text input w Grafikach nie został znaleziony")
+                print(f'---- {link["image_link"]}  ------   {link["vin"]} --------')
+
             driver.find_element(By.XPATH, button_send_block).click()
             try:
                 sleep(1)
@@ -244,18 +244,16 @@ def block_images_request(driver, links):
             except Exception:
                 pass
 
-            try:
-                page_exists_block_address_button = WebDriverWait(driver, 3).until(
-                    lambda d: d.find_element(By.XPATH, page_exists_block_address_xpath))
+            try:  # i przeslij prosbe albo jestt ok to tylko wtedy rzuc exeption jesli nie ma
+                page_exists_block_address_button = WebDriverWait(driver, 4).until(lambda d: d.find_element(By.XPATH, page_exists_block_address_xpath))
                 page_exists_block_address_button.send_keys(link["vin"])
             except Exception as e:
-                print(f"Exception  --------------------- block_images_request")
                 try:
-                    driver.find_element(By.XPATH, '//span[text()="Prześlij prośbę"]').click()
+                    driver.find_element(By.XPATH, button_send_block_request).click()
                 except Exception:
                     pass
                 try:
-                    request_send_correctly_button = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, request_send_correctly))
+                    request_send_correctly_button = WebDriverWait(driver, 3).until(lambda d: d.find_element(By.XPATH, request_send_correctly))
                     request_send_correctly_button.click()
                 except Exception:
                     pass
@@ -272,17 +270,15 @@ if __name__ == "__main__":
     # load_vins()
     driver = init_driver()
     login(driver)
-    first_search(driver)
+    # first_search(driver)
 
     for vin in VINS:
         new_search(driver, vin)
         print(f"______________________________________________________vin: {vin}")
         page_links = links_scrapper(driver, vin)
         print(f"_____________________________________________________________________________page_links       len: {len(page_links)}")
-        print(f"______________________________________________________page_links: {page_links}")
         images_links = images_scrapper(driver, vin)
-        print(f"_____________________________________________________________________________image_links      len: {len(images_links)}")
-        print(f"______________________________________________________image_links: {images_links}")
+        print(f"___________________________________________________________________________images_links       len: {len(images_links)}")
         if page_links:
             block_pages_request(driver, page_links)
         if images_links:
@@ -303,5 +299,5 @@ if __name__ == "__main__":
     #     block_pages_request(driver, PAGES_LINKS)
     # except Exception:
     #     ipdb.set_trace()
-    ipdb.set_trace()
+    # ipdb.set_trace()
     driver.quit()
